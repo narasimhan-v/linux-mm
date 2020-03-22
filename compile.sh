ff7e7262b480b87138f1689a8cfbfbd7e4eecc2f
Qian Cai <cai@lca.pw>
2020-03-22 00:07:59 -0400

    compile.sh: install packages for virtualization
    To enable KVM and runc tests.
    
    Signed-off-by: Qian Cai <cai@lca.pw>

--- compile.sh
+++ compile.sh
@@ -44,7 +44,7 @@ if [ ! -f .config ]; then
 	yum -y install openssl-devel make gcc bc bison flex ncurses-devel \
 	    autoconf automake libaio-devel libattr-devel libcap-devel \
 	    libgcrypt-devel keyutils-libs zlib-devel elfutils-libelf-devel \
-	    grubby wget tar patch time sysstat jq
+	    grubby wget tar patch time sysstat jq runc qemu-kvm genisoimage
 
 	if [[ "$arch" != 's390x' ]]; then
 		yum -y install numactl-devel numactl
