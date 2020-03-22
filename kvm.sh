c43c75ac0c6f20ed40e58f917908a0b09aa35550
Qian Cai <cai@lca.pw>
2020-03-22 00:25:30 -0400

    kvm.sh: add KVM tests
    Add some basic tests to start up an Ubuntu cloud image VM. Currently,
    only x86 and powerpc are supported.
    
    Signed-off-by: Qian Cai <cai@lca.pw>

---
+++ kvm.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -eux -o pipefail
+
+arch=$(uname -m)
+distro='ubuntu-18.04-server-cloudimg'
+
+if [ ! -f "$distro.qcow2" ]; then
+	case "$arch" in
+	'ppc64le')
+		image="$distro-ppc64el.img"
+		;;
+	'x86_64')
+		image="$distro-amd64.img"
+	esac
+	if [ ! -f "$image" ]; then
+		curl -O "https://cloud-images.ubuntu.com/releases/bionic/release/$image"
+	fi
+	qemu-img create -b "$image" -f qcow2 "$distro.qcow2" 1T
+fi
+if [ ! -f "$distro.iso" ]; then
+	cat > meta-data <<-EOF
+	instance-id: $distro
+	local-hostname: $distro
+EOF
+	cat > user-data <<-EOF
+	#cloud-config
+	password: $distro
+	chpasswd: { expire: False }
+	ssh_pwauth: True
+EOF
+	genisoimage -output "$distro.iso" -volid cidata -joliet -rock \
+		user-data meta-data
+fi
+/usr/libexec/qemu-kvm -name "$distro" -cpu host -smp 2 -m 2G \
+	-hda "$distro.qcow2" -cdrom "$distro.iso" \
+	-nic user,hostfwd=tcp::2222-:22 -serial mon:stdio
